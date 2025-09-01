51b02911206d5025bd148f13bb55d3e96705c2ee
KrishMakhija <krishmakhija0603@gmail.com>
2025-09-01 15:58:21 +0530

    Create Object-Declaration.java

---
+++ Object-Declaration.java
@@ -0,0 +1,21 @@
+class Krish
+{
+int x=5;
+void f1()
+{
+
+System.out.println();
+}
+
+}
+class Koushal
+{
+public static void main(String[]largs)
+{
+Krish s1= new Krish();
+System.out.println(s1.x);
+
+}
+}
+
+
