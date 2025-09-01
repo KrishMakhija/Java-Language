1330588fa6fd061f8a155873361cae92f50c9a7b
KrishMakhija <krishmakhija0603@gmail.com>
2025-09-01 16:04:25 +0530

    Create Private-Class.java

---
+++ Private-Class.java
@@ -0,0 +1,18 @@
+class Krish
+{
+private int x=1;
+void f1()
+{
+
+System.out.println(x);
+}
+
+}
+class Koushal
+{
+public static void main(String[]largs)
+{
+Krish s1= new Krish();
+s1.f1();
+}
+}
